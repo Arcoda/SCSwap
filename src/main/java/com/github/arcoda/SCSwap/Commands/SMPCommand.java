406dff14d65b7be8ef45f036f3c21e6399055742
Movs22 <111647781+Movs22@users.noreply.github.com>
2022-09-29 20:24:01 +0100

    Update SMPCommand.java
    Added bannedList: a list containing all players who are blocked from the smp. Use /block to add/remove members from that list.
    Changed the command so it would not teleport if the player was banned from the smp.

--- src/main/java/com/github/arcoda/SCSwap/Commands/SMPCommand.java
+++ src/main/java/com/github/arcoda/SCSwap/Commands/SMPCommand.java
@@ -1,13 +1,31 @@
 package com.github.arcoda.SCSwap.Commands;
 import com.github.arcoda.SCSwap.SCSwap;
+
+import net.md_5.bungee.api.ChatColor;
+
+import java.util.List;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 
 public class SMPCommand implements CommandExecutor {
-    @Override
+	@SuppressWarnings("unused")
+	private SCSwap plugin;
+
+	public SMPCommand (SCSwap plugin) {
+		this.plugin = plugin;
+		plugin.getCommand("smp").setExecutor(this);
+	}	
+	@Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        return SCSwap.getInstance().getTeleportLib.teleportTo(sender, "Survival");
+    	List<String> bannedList = (List<String>) this.plugin.getConfig().getList("smp.Blocked");
+    	if(bannedList.contains(sender.getName())) {
+    		sender.sendMessage(ChatColor.RED + "You have been banned from the SMP. Open a ticket if you wish to appeal your ban.");
+    		return true;
+    	} else {
+    		return SCSwap.getInstance().getTeleportLib.teleportTo(sender, "Survival");
+    	}
     }
 }
