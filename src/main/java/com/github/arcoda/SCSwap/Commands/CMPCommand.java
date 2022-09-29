91c51e37b423b933722ecd8577691583550a4f15
Movs22 <111647781+Movs22@users.noreply.github.com>
2022-09-29 20:22:57 +0100

    Update CMPCommand.java
    Updated /cmp plugin to follow the format of the other commands.
    Added this.plugin so it can load the config files (not used in this command)

--- src/main/java/com/github/arcoda/SCSwap/Commands/CMPCommand.java
+++ src/main/java/com/github/arcoda/SCSwap/Commands/CMPCommand.java
@@ -6,6 +6,14 @@ import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 
 public class CMPCommand implements CommandExecutor {
+	@SuppressWarnings("unused")
+	private SCSwap plugin;
+
+	public CMPCommand (SCSwap plugin) {
+		this.plugin = plugin;
+		plugin.getCommand("cmp").setExecutor(this);
+	}
+
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         return SCSwap.getInstance().getTeleportLib.teleportTo(sender, "Creative");
