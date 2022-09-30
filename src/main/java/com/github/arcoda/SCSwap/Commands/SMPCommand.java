70325f56f710ea5294e3bede226795ee081d9fe6
Movs22 <111647781+Movs22@users.noreply.github.com>
2022-09-30 17:24:15 +0100

    Update SMPCommand.java
    Implemented bannedList, blocking anyone on the list from using the command to get into the SMP.

--- src/main/java/com/github/arcoda/SCSwap/Commands/SMPCommand.java
+++ src/main/java/com/github/arcoda/SCSwap/Commands/SMPCommand.java
@@ -1,13 +1,26 @@
 package com.github.arcoda.SCSwap.Commands;
 import com.github.arcoda.SCSwap.SCSwap;
+
+import net.md_5.bungee.api.ChatColor;
+
+import java.util.List;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 
 public class SMPCommand implements CommandExecutor {
-    @Override
+	
+	@Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        return SCSwap.getInstance().getTeleportLib.teleportTo(sender, "Survival");
+    	@SuppressWarnings("unchecked")
+		List<String> bannedList = (List<String>) SCSwap.getInstance().getConfig().getList("smp.Blocked");
+    	if(bannedList.contains(sender.getName())) {
+    		sender.sendMessage(ChatColor.RED + "You have been banned from the SMP. Open a ticket if you wish to appeal your ban.");
+    		return true;
+    	} else {
+    		return SCSwap.getInstance().getTeleportLib.teleportTo(sender, "Survival");
+    	}
     }
 }
